f1766ac68b8eb085a535092d1310d5cca788c08f
Victor Severin <58826322+VictorSeverin@users.noreply.github.com>
2023-11-28 21:06:02 -0800

    Update openai.js

--- src/server/openai.js
+++ src/server/openai.js
@@ -2,7 +2,6 @@
 import OpenAI from 'openai';
 
 export const generateResponse = async (promptText) => {
-    // Define your API request payload based on the requirements of the OpenAI API
 
     const openai = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY,
